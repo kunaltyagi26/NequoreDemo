cabeacbae907a14857b86e1a6b345593792c7e47
kunaltyagi26 <kunaltyagi32@gmail.com>
2019-01-13 19:56:10 +0530

    Updated code.

--- Nequore/Controller/HomeVC.swift
+++ Nequore/Controller/HomeVC.swift
@@ -18,8 +18,6 @@ class HomeVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    //var categories: [String] = ["Recommended", "Top Developers", "Pre-Sales", "Popular Projects", "Featured Localities"]
-    
     var data: Data?
     var recommended: [Recommended] = []
     var topDevelopers: [TopDevelopers] = []
@@ -137,21 +135,6 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let category: String = Category.categories[indexPath.section]
         
-        /*switch indexPath.section {
-        case 0:
-            return self.view.frame.width * 0.80
-        case 1:
-            return self.view.frame.width * 0.50
-        case 2:
-            return self.view.frame.width * 0.50
-        case 3:
-            return self.view.frame.width * 0.40
-        case 4:
-            return self.view.frame.width * 0.40
-        default:
-            return 0
-        }*/
-        
         if category == "Recommended" {
             return self.view.frame.width * 0.80
         } else if category == "Top Developers" {
@@ -183,21 +166,6 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let index = collectionView.tag
         let category: String = Category.categories[index]
-        
-        /*switch index {
-        case 0:
-            return recommended.count
-        case 1:
-            return topDevelopers.count
-        case 2:
-            return preSale.count
-        case 3:
-            return popularProjects.count
-        case 4:
-            return featuredLocalities.count
-        default:
-            return 0
-        }*/
         
         if category == "Recommended" {
             return recommended.count
@@ -283,46 +251,6 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource {
             cell = UICollectionViewCell()
         }
         
-        /*switch index {
-        case 0:
-            guard let recommendedCell = collectionView.dequeueReusableCell(withReuseIdentifier: "recommendedCell", for: indexPath) as? RecommendedCell else { return UICollectionViewCell() }
-            
-            recommendedCell.configureCell(imageUrl: URL(string: recommendedObj.image ?? "https://httpbin.org/image/png")!, address: recommendedObj.city?.name ?? "", description: recommendedObj.descriptionValue ?? "", price: recommendedObj.minPrice ?? 0)
-            
-            cell = recommendedCell
-        case 1:
-            /*guard let topDevelopersCell = collectionView.dequeueReusableCell(withReuseIdentifier: "topDevelopersCell", for: indexPath) as? TopDevelopersCell else { return UICollectionViewCell() }
-            
-            topDevelopersCell.configureCell(imageUrl: URL(string: topDeveloper.image ?? "https://httpbin.org/image/png")!, numberOfProjects: topDeveloper.buildingsCount ?? 0, developer: topDeveloper.name ?? "", developerDesc: topDeveloper.developerTitle ?? "")
-            
-            cell = topDevelopersCell*/
-            guard let recommendedCell = collectionView.dequeueReusableCell(withReuseIdentifier: "recommendedCell", for: indexPath) as? RecommendedCell else { return UICollectionViewCell() }
-            
-            recommendedCell.configureCell(imageUrl: URL(string: recommendedObj.image ?? "https://httpbin.org/image/png")!, address: (recommendedObj.city?.nameEn)!, description: recommendedObj.descriptionValue!, price: recommendedObj.maxPrice!)
-            
-            cell =  recommendedCell
-        case 2:
-            guard let preSalesCell = collectionView.dequeueReusableCell(withReuseIdentifier: "preSalesCell", for: indexPath) as? PreSalesCell else { return UICollectionViewCell() }
-            
-            preSalesCell.configureCell(imageUrl: URL(string: preSaleObj.mainImage ?? "https://httpbin.org/image/png")!, price: (preSaleObj.startingPrice ?? 0) / 1000, desc: preSaleObj.configurations?[0].config?.name ?? "", owner: preSaleObj.name ?? "")
-            
-            cell = preSalesCell
-        case 3:
-            guard let popularProjectsCell = collectionView.dequeueReusableCell(withReuseIdentifier: "popularProjectsCell", for: indexPath) as? PopularProjectsCell else { return UICollectionViewCell() }
-            
-            popularProjectsCell.configureCell(imageUrl: URL(string: popularProj.mainImage ?? "https://httpbin.org/image/png")!, owner: popularProj.developer?.name ?? "", projectName: popularProj.name ?? "", desc: popularProj.configurations?[0].config?.name ?? "", price: (popularProj.startingPrice ?? 0) / 1000)
-            
-            cell = popularProjectsCell
-        case 4:
-            guard let featuredLocalitiesCell = collectionView.dequeueReusableCell(withReuseIdentifier: "featuredLocalitiesCell", for: indexPath) as? FeaturedLocalitiesCell else { return UICollectionViewCell() }
-            
-            featuredLocalitiesCell.configureCell(locality: featLoc.name ?? "", price: featLoc.pricePerSqft ?? 0, numberofProjects: featLoc.buildingsCount ?? 0)
-            
-            cell = featuredLocalitiesCell
-        default:
-            cell = UICollectionViewCell()
-        }*/
-        
         cell.layer.masksToBounds = false
         cell.layer.shadowColor = UIColor.black.cgColor
         cell.layer.shadowOpacity = 0.3
@@ -358,21 +286,6 @@ extension HomeVC: UICollectionViewDelegateFlowLayout {
         else {
             itemWidth = 0
         }
-        
-        /*switch index {
-        case 0:
-            itemWidth = self.view.bounds.width * 0.7
-        case 1:
-            itemWidth = self.view.bounds.width * 0.5
-        case 2:
-            itemWidth = self.view.bounds.width * 0.9
-        case 3:
-            itemWidth = self.view.bounds.width * 0.8
-        case 4:
-            itemWidth = self.view.bounds.width * 0.8
-        default:
-            itemWidth = 0
-        }*/
         
         let itemHeight = collectionView.bounds.height - (2 * padding)
         return CGSize(width: itemWidth, height: itemHeight)
